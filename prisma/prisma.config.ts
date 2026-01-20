78ac13d6f0e3cd723b7dfc6424bea110ccf62777
surajc-15 <surajchanaveeragoudra@gmail.com>
2026-01-21 02:51:57 +0530

    feat: Add Prisma configuration and automate schema generation with a postinstall script.lli

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,12 +1,19 @@
-// prisma.config.ts
+// // prisma.config.ts
+// import { defineConfig } from "prisma/config";
+
+// export default defineConfig({
+//     migrations: {
+//         path: "prisma/migrations",
+//     },
+//     db: {
+//         adapter: "postgresql",
+//         url: process.env.DATABASE_URL!,
+//     },
+// });
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
     migrations: {
         path: "prisma/migrations",
-    },
-    db: {
-        adapter: "postgresql",
-        url: process.env.DATABASE_URL!,
     },
 });
